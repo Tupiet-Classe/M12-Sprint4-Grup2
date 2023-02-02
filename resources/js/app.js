33f92b906c59886fe36d610788b5a818c283ab24
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-02 19:32:14 +0100

    proves VUE

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,13 @@ import App from '../vue/App.vue'
 
 import Prueba from '../vue/Prueba.vue';
 
+import Perfil from '../vue/Perfil.vue';
+
 const app = createApp({})
 
 app.component('andrei-prova', Prueba)
+
+app.component('perfil-andrei', Perfil)
+
 
 app.mount("#app")
---
+++ resources/vue/Perfil.vue
@@ -0,0 +1,17 @@
+<!-- Codi de la pagina -->
+<template>
+    <div>
+        <h1>wehjdvbddm</h1>
+    </div>
+</template>
+
+
+<!-- Estils per a esta vista -->
+<style scoped>
+
+</style>
+
+<!-- Scripts per a esta vista -->
+<script>
+
+</script>
