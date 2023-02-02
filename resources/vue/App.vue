90d21f24066e2c6123264a73a5321c5db6d9f3d1
Tupiet <aleixescrihuela@gmail.com>
2023-02-02 16:25:01 +0100

    Vue afegit

---
+++ resources/vue/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1> Hello, Vuejs with Laravel </h1>
+</template>
+
+<script>
+    export default {
+        setup() {
+
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        background: red;
+    }
+</style>
