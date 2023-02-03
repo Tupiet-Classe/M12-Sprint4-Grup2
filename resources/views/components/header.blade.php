47faa1700e0db234992d54f9abd56864edfc2970
Tupiet <aleixescrihuela@gmail.com>
2023-02-03 15:18:35 +0100

    Petits canvis visuals

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header>
-    <button><i class="fa fa-user p"></i></button>
-    <button><i class="fa fa-comments"></i></button>
+<header class="flex justify-between sm:hidden mt-3 mx-5">
+    <button><i class="fa fa-user p-1 text-2xl bg-slate-200 text-morado"></i></button>
+    <button><i class="fa fa-comments p-1 text-2xl bg-slate-200 text-morado"></i></button>
 </header>
